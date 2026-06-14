dfbdf652b6b7d7694fd0ce535bf67cdfaa99b788
Bhavya Singh <bhavya1singh1@gmail.com>
2026-06-14 17:45:15 +0530

    added a way for installing dependencies

---
+++ depen.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/Octahedron-apple/PlaneMCP.git
+# The MCP 
+git clone https://github.com/Octahedron-apple/LineRun.git
+# The code runner
+git clone https://github.com/Octahedron-apple/DotDB.git
+# The database 
---
+++ src/main.py
@@ -0,0 +1,5 @@
+import linerun
+import dotdb
+import planemcp
+class AI_Agent:
+
