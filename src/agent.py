1f9f4728c0ff9cc95bc9ba089ebdfe0fb926b00a
Bhavya Singh <bhavya1singh1@gmail.com>
2026-06-19 18:38:36 +0530

    Made the init method and add user message method

--- src/agent.py
+++ src/agent.py
@@ -1,5 +1,5 @@
-import json
 import asyncio
+import json
 import time
 
 try:
@@ -7,9 +7,23 @@ try:
 except ImportError:
     pass
 
-class AI_Agent:
-    """
 
-    """
+class AI_Agent:
+    """ """
+
     def __init__(self, Runner) -> None:
-        pass
+        self.Runner = Runner
+        self.Running = False
+        self.Api_Key = ""
+        self.Model = ""
+        self.System_Prompt = "You Are an AI Agent. You have access to an python Sandbox. Use the tools provided to you to fullfill the request of the user."
+        self.Messages = [{"role": "system", "content": self.System_Prompt}]
+
+    def Set_Config(self, Api_key, Model):
+        """ """
+        self.Api_Key = Api_key
+        self.Model = Model
+
+    def Add_User_Message(self, text):
+        """ """
+        self.Messages.append({"role": "user", "content": text})
